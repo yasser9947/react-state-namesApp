0754e24c36f11c4a9e01cc762046afa6490e8570
Yasir S <yasser9946.1@gmail.com>
2019-10-27 01:34:24 +0300

    added files

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import './App.css'
+
+export default class App extends Component {
+
+
+
+
+  
+
+  render() {
+    return (
+      <div>
+        <div className="contenerpord">
+
+      test
+
+        </div>
+      </div>
+    )
+  }
+}
+
+
---
+++ src/Data.js
@@ -0,0 +1,73 @@
+var Data = [
+    {
+        name: "Yasir",
+        Age: 26,
+        city: "zulfi",
+        Nationality: "Saudi",
+        Major: "Mathematics",
+        img: "https://www.vippng.com/png/detail/24-243886_death-note-l-l-death-note-png.png",
+        contact: "09665666666",
+        email: "yasir.almuhtrish@generalassemb.ly",
+    }
+    , {
+        name: "Ebere",
+        Age: 43,
+        city: "Yuexiu",
+        Nationality: "Nigerian",
+        Major: "Computer science",
+        img: "https://ca.slack-edge.com/TA2AHQDQ8-UFH2RV1EJ-22be39ae6f75-512",
+        contact: "09665555555",
+        email: "ebere@generalassemb.ly",
+    }
+    , {
+        name: "Ahmed",
+        Age: 23,
+        city: "Makkah",
+        Nationality: "Saudi",
+        img: "https://ca.slack-edge.com/TA2AHQDQ8-UFAQDSARK-1c276daaa017-512",
+        Major: "Chef",
+        contact: "09665444444",
+        email: "Ahmed@generalassemb.ly",
+    }
+    , {
+        name: "Atheer",
+        Age: 21,
+        city: "Istanbul",
+        Nationality: "Turkey",
+        Major: "Cardiologist",
+        img: "https://cdn.pixabay.com/photo/2015/03/17/14/05/sparkler-677774_960_720.jpg",
+        contact: "09665333333",
+        email: "ebere@generalassemb.ly",
+    }, {
+        name: "Mohhamed",
+        Age: 27,
+        city: "Zulfi",
+        Nationality: "Saudi",
+        Major: "Linguistics",
+        img: "https://instagram.fjed6-1.fna.fbcdn.net/vp/398655821f9f2b356dad0806ea17fa57/5E565B45/t51.2885-19/s150x150/70408580_427932754500397_8675784331047731200_n.jpg?_nc_ht=instagram.fjed6-1.fna.fbcdn.net",
+        contact: "09665222222",
+        email: "mohhamed@generalassemb.ly",
+    }, {
+        name: "Jehad",
+        Age: 29,
+        city: "unknown",
+        Major: "unknown",
+        img: "https://cdn.pixabay.com/photo/2014/09/03/20/15/legs-434918_960_720.jpg",
+        contact: "09665111111",
+        email: "unknown@generalassemb.ly",
+    }
+]
+export default Data
+
+
+
+
+
+
+
+
+
+
+
+
+
